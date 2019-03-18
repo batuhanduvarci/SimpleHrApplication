a385a93fff8a85025c1dff149bf7258680a61329
batuhanduvarci <batuhanduvarci@gmail.com>
2019-03-18 23:48:01 +0300

    Initial commit: Project generated.

---
+++ src/main/java/com/example/batuhanduvarci/demo/SimpleHrApplication.java
@@ -0,0 +1,13 @@
+package com.example.batuhanduvarci.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHrApplication.class, args);
+	}
+
+}
